48f11ce9b07ae331dc63f52d2ac454a261a760cd
Jaime <jaidelav@ucm.es>
2022-09-22 13:54:21 +0200

    Inicio del código para la interfaz en consola con Python

---
+++ chess.py
@@ -0,0 +1,19 @@
+class Chess():
+    def __init__(self):
+        pass
+class Board():
+    def __init__(self):
+        pass
+class Piece():
+    def __init__(self):
+        pass
+
+class Knight(Piece):
+    def __init__(self):
+        pass
+
+class Pawn(Piece):
+    def __init__(self):
+        pass
+    
+
