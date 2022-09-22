90cb2d8763b7239c7974381570e4fbdda60b890a
Jaime <jaidelav@ucm.es>
2022-09-22 14:29:25 +0200

    idk

--- chess.py
+++ chess.py
@@ -1,43 +1,72 @@
 class Chess():
     def __init__(self):
-        self.board = board.Board()
+        
+
+
 class Board():
+    
     def __init__(self):
         pass
+    
     def print_board(self):
         pass
-
+        
 
 class Piece():
+    
     def __init__(self):
+        pass
+    
+    def position():
         pass
 
     def is_white(self):
         pass
 
 class Knight(Piece):
-    def __init__(self):
+    
+    def __init__(self,x,y):
+        
+    
+    def is_valid_move(self):
         pass
 
 class Pawn(Piece):
+    
     def __init__(self):
         pass
     
+    def is_valid_move(self):
+        pass    
+
 class Bishop(Piece):
+    
     def __init__(self):
+        pass
+    
+    def is_valid_move(self):
         pass
 
 class Rook(Piece):
+    
     def __init__(self):
         pass
-
+    
+    def is_valid_move():
+        pass
 
 class Queen(Piece):
+    
     def __init__(self):
         pass
-
+    
+    def is_valid_move(self):
+        pass
 
 class King(Piece):
+    
     def __init__(self):
         pass
-
+    
+    def is_valid_move(self):
+        pass
