1e3694bb93bb05b2efe11683d74e40b3dfcd152d
Jaime <jaidelav@ucm.es>
2022-09-22 14:04:00 +0200

    Todas las piezas incluidas

--- chess.py
+++ chess.py
@@ -1,11 +1,18 @@
 class Chess():
     def __init__(self):
-        pass
+        self.board = board.Board()
 class Board():
     def __init__(self):
         pass
+    def print_board(self):
+        pass
+
+
 class Piece():
     def __init__(self):
+        pass
+
+    def is_white(self):
         pass
 
 class Knight(Piece):
@@ -16,4 +23,21 @@ class Pawn(Piece):
     def __init__(self):
         pass
     
+class Bishop(Piece):
+    def __init__(self):
+        pass
+
+class Rook(Piece):
+    def __init__(self):
+        pass
+
+
+class Queen(Piece):
+    def __init__(self):
+        pass
+
+
+class King(Piece):
+    def __init__(self):
+        pass
 
